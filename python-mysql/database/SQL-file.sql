8fc8391f1e4ff2d3559162589e7dfcd5d588d4b5
Neldis Barrios <neldis12@hotmail.com>
2020-11-13 20:35:34 +0100

    Update SQL-file.sql

--- python-mysql/database/SQL-file.sql
+++ python-mysql/database/SQL-file.sql
@@ -2,7 +2,12 @@ create database dbclient;
 
 use dbclient;
 
-create table Customer(id int, first_name varchar(30) not null, last_name varchar(30) not null, dni varchar(9) not null);
+create table Customer(
+             id int, 
+             first_name varchar(30) not null, 
+             last_name varchar(30) not null, 
+             dni varchar(9) not null
+             );
 
 alter table Customer add unique (dni);
 
