e0f2b56b12c47bc6b730a758bd59f9f9634d8092
Neldis Barrios <neldis12@hotmail.com>
2020-11-14 14:56:12 +0100

    Update SQL-file.sql

--- python-mysql/database/SQL-file.sql
+++ python-mysql/database/SQL-file.sql
@@ -3,15 +3,12 @@ create database dbclient;
 use dbclient;
 
 create table Customer(
-             id int, 
+             id int not null auto_increment primary key, 
              first_name varchar(30) not null, 
              last_name varchar(30) not null, 
              dni varchar(9) not null
              );
 
-alter table Customer add unique (dni);
+alter table Customer add unique (id,dni);
 
-alter table Customer add primary key (id);
-
-alter table Customer modify column id int auto_increment;
 
