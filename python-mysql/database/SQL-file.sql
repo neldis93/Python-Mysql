8c0871b7ee272828eb5d45c650c17aeba01a51b7
Neldis Barrios <neldis12@hotmail.com>
2020-11-15 00:49:29 +0100

    Update SQL-file.sql

--- python-mysql/database/SQL-file.sql
+++ python-mysql/database/SQL-file.sql
@@ -11,24 +11,3 @@ create table Customer(
 
 alter table Customer add unique (dni);
 
-<<<<<<< HEAD
-
-alter table Customer add primary key (id);
-
-alter table Customer drop column primary key;
-describe Customer;
-
-alter table Customer modify column id int auto_increment;
-=======
->>>>>>> cf35db830312eb99598af623ce7f31f30344566e
-
-insert into Customer (first_name,last_name,dni)
-values('Neldis','Barrios', 'Y34FRDS43');
-
-alter table Customer drop column id;
-
-delete from Customer where concat(id,first_name, last_name,dni) is null; 
-
-delete from Customer where dni= 'Y7401678W';
-
-update Customer set last_name=('Barrios') where last_name=('Heredia');
