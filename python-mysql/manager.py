10876d20ad7fa58458978b7cf14ae0965e0ba78d
Neldis Barrios <neldis12@hotmail.com>
2021-11-04 10:47:07 +0100

    Syntax and conditionals

--- python-mysql/manager.py
+++ python-mysql/manager.py
@@ -1,21 +1,18 @@
 from database.connection import DAO
 
-
 def listclients(clients):
     #cont=1
     for i in clients:
-        data= "ID:{0}.- DNI: {1}  | First name: {2}  | Last name: {3}"
+        data = "ID:{0}.- DNI: {1}  | First name: {2}  | Last name: {3}"
         print(data.format(i[0], i[3], i[1], i[2] ))
         #cont +=1
     print(" ")
 
-
-
 def is_valid_client():
     while True:
-        first_name= str(input('Please enter your first name: '))
-        last_name=str(input('Please enter your last name: '))
-        dni= str(input('Enter your DNI: ').upper())
+        first_name = str(input('Please enter your first name: '))
+        last_name = str(input('Please enter your last name: '))
+        dni = str(input('Enter your DNI: ').upper())
         print(" ")
         if len(first_name) > 0 and  len(last_name) > 0 and len(dni) > 0:
             if len(dni) == 9:
@@ -27,36 +24,30 @@ def is_valid_client():
             print('The first name, last name and DNI field cannot be empty.')
             print('Please login again\n')
     
-    client= (first_name,last_name,dni)
+    client = (first_name,last_name,dni)
     return client
-
-    
 
 def data_deletion(clients):
     listclients(clients)
-    exists_dni=False
-   
-    client_to_delete= input('Enter the DNI of the customer to remove: ')
+    exists_dni = False
+    client_to_delete = input('Enter the DNI of the customer to remove: ')
         
     for client in clients:
         if client[3] == client_to_delete:
-            exists_dni=True
+            exists_dni = True
             break
         
     if not exists_dni:
-        client_to_delete= ""
+        client_to_delete = ""
 
     return client_to_delete
 
-
-
-
 def update_data(clients):
-    dao= DAO()
+    dao = DAO()
     listclients(clients)
-    exists_id=False
+    exists_id = False
     while not (exists_id):
-        client_id=int(input('Enter the ID you want to update: '))    
+        client_id = int(input('Enter the ID you want to update: '))    
         for i in clients:
             if i[0] == client_id:
                 print(dao.find_client(client_id))
@@ -65,11 +56,10 @@ def update_data(clients):
         if not exists_id:
             print('***Id not found***')
 
-
-    client_first_n= input('Enter new name: ')
-    client_last_n= input('Enter new last name: ')
+    client_first_n = input('Enter new name: ')
+    client_last_n = input('Enter new last name: ')
     while True:
-        client_dni= input('Enter new DNI: ')
+        client_dni = input('Enter new DNI: ')
         print("")
         if len(client_dni) == 9:
             break
@@ -77,8 +67,4 @@ def update_data(clients):
             print('***The DNI field cannot have more or less than 9 alphanumeric characters.***')
             print('Please login again\n')
         
-
     return dao.update_client(client_id,client_first_n,client_last_n,client_dni)
-
-
-
