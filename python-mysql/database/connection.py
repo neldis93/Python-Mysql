d4c4b1ab2dbde266e22eca9a6286b5c7c978c6e7
Neldis Barrios <neldis12@hotmail.com>
2021-11-04 10:56:59 +0100

    Code settings

--- python-mysql/database/connection.py
+++ python-mysql/database/connection.py
@@ -4,24 +4,21 @@ from .base import get_secret
 
 
 class DAO:
-
     def __init__(self):
         try:
-            self.myconnection= mysql.connector.connect(
-                port=get_secret('PORT'),
-                user=get_secret('USER'),
-                password=get_secret('PASSWORD'),
-                database=get_secret('DATABASE'),  
-                auth_plugin=get_secret('AUTH_PLUGIN')
+            self.myconnection = mysql.connector.connect(
+                port = get_secret('PORT'),
+                user = get_secret('USER'),
+                password = get_secret('PASSWORD'),
+                database = get_secret('DATABASE'),  
+                auth_plugin = get_secret('AUTH_PLUGIN')
             )
             # if self.myconnection.is_connected():
             #     print('Successful connection')
-
-                
+            
         except Error as ex:
             print(f'Error during connection:{ex}')
-
-
+            
         # finally:
         #     if self.myconnection.is_connected():
         #         self.myconnection.close()
@@ -30,9 +27,9 @@ class DAO:
     def list_client(self):
         if self.myconnection.is_connected():
             try:
-                cursor= self.myconnection.cursor()
+                cursor = self.myconnection.cursor()
                 cursor.execute("SELECT * FROM Customer ORDER BY id ASC")
-                result=cursor.fetchall()
+                result = cursor.fetchall()
                 return result
 
             except Error as ex:
@@ -41,21 +38,20 @@ class DAO:
     def find_client(self,Id):
         if self.myconnection.is_connected():
             try:
-                cursor= self.myconnection.cursor()
-                sql="SELECT * FROM Customer WHERE id={}"
+                cursor = self.myconnection.cursor()
+                sql = "SELECT * FROM Customer WHERE id={}"
                 cursor.execute(sql.format(Id))
-                result=cursor.fetchone()
+                result = cursor.fetchone()
                 return result
 
             except Error as ex:
                 print(f'Error during connection:{ex}')
 
-
     def register_clients(self,values):
         if self.myconnection.is_connected():
             try:
-                cursor= self.myconnection.cursor()
-                sql= "INSERT INTO Customer (first_name,last_name,dni) VALUES ('{0}','{1}','{2}')"
+                cursor = self.myconnection.cursor()
+                sql = "INSERT INTO Customer (first_name,last_name,dni) VALUES ('{0}','{1}','{2}')"
                 cursor.execute(sql.format(values[0], values[1],values[2]))
                 self.myconnection.commit()
                 print("")
@@ -64,12 +60,11 @@ class DAO:
             except Error as ex:
                 print(f'Error during connection:{ex}')  
 
-
     def update_client(self,Id,first_name,last_name,dni):
         if self.myconnection.is_connected():
             try:
-                cursor= self.myconnection.cursor()
-                sql= "UPDATE Customer SET first_name='{}',last_name='{}',dni='{}' WHERE id={}"
+                cursor = self.myconnection.cursor()
+                sql = "UPDATE Customer SET first_name='{}',last_name='{}',dni='{}' WHERE id={}"
                 cursor.execute(sql.format(first_name,last_name,dni,Id))
                 self.myconnection.commit()
                 print("Customer updated\n")
@@ -77,13 +72,11 @@ class DAO:
             except Error as ex:
                 print(f'Error during connection:{ex}') 
 
-
-
     def delete_client(self,code_delete):
         if self.myconnection.is_connected():
             try:
-                cursor= self.myconnection.cursor()
-                sql= "DELETE FROM Customer WHERE dni= '{0}'"
+                cursor = self.myconnection.cursor()
+                sql = "DELETE FROM Customer WHERE dni= '{0}'"
                 cursor.execute(sql.format(code_delete))
                 self.myconnection.commit()
                 print("Customer removed\n")
